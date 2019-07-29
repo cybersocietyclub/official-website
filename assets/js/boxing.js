b167ad7ab40a7ce3eb99933115fa007989f835e5
VMrGamer <v.mr.gamer@gmail.com>
2019-07-29 23:49:44 +0530

    Javascript Required for the gallery

---
+++ assets/js/boxing.js
@@ -0,0 +1,54 @@
+var Boxlayout = function() {
+
+  var wrapper = document.body,
+      sections = Array.from(document.querySelectorAll('.gallery--grid-section')),
+      closeButtons = Array.from(document.querySelectorAll('.gallery--grid-section-closed')),
+      header = document.querySelector('.header'),
+      sideNav = document.querySelector('.l-side-nav'),
+      banner = document.querySelector('.gallery--banner'),
+      expandedClass = 'gallery--grid-section-is-expanded',
+      hasExpandedClass = 'gallery--grid-section-has-expanded-item';
+
+  return { init : init };
+
+  function init() {
+    _initEvents();
+  }
+
+  function _initEvents() {    
+    sections.forEach(function(element) {
+      element.onclick = function() {
+        _openSection(this);
+      };
+    });
+    closeButtons.forEach(function(element) {
+      element.onclick = function(element) {
+        element.stopPropagation();
+        _closeSection(this.parentElement);
+      };
+    });
+  }
+
+  function _openSection(element) {
+    if ( ! element.classList.contains(expandedClass) ) {
+      element.classList.add(expandedClass);
+      wrapper.classList.add(hasExpandedClass);
+      header.style.display = 'none';
+      sideNav.style.display = 'none';
+      banner.style.display = 'none';
+    }
+  }
+
+  function _closeSection(element) {
+    if ( element.classList.contains(expandedClass) ) {
+      element.classList.remove(expandedClass);
+      wrapper.classList.remove(hasExpandedClass);
+      header.style.display = 'flex';
+      sideNav.style.display = 'flex';
+      banner.style.display = 'block';
+    }
+  }
+
+}();
+
+Boxlayout.init();
